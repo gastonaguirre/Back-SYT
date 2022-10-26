a020ff9a8143e354e73ee8e335ad4bb0d2081758
Luis Condori <luis2003nb@gmail.com>
2022-10-26 13:29:25 -0300

    add(ft/controllers/post.js) and (ft/posts/posts.js) and (ft/app.js)

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-const { getAllPost } = require('../controllers/posts');
+const { getAllPost , postOFPost, detailPost, eliminarPost} = require('../controllers/posts');
 
 
 const router = Router();
 
 router.get('/', getAllPost);
+router.get("/:id", detailPost)
+router.post("/", postOFPost)
+router.delete("/:id",eliminarPost)
 
 module.exports = router;
