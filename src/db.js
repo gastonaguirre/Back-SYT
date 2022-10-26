67795769947564f2aba38800dbcbdb6218568d62
gaston <ztonchyarg@hotmail.com>
2022-10-26 11:56:51 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -2,21 +2,35 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/syt`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-
-sequelize.authenticate()
-.then (()=>console.log('base de datos conectada'))
-.catch ((e)=>console.log('error no conecta la base de datos',e))
-
-
-
+let sequelize =
+  process.env.NODE_ENV === 'production'
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: 'postgres',
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          },
+          keepAlive: true
+        },
+        ssl: true
+      })
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/syt`, {
+        logging: false,
+        native: false
+      });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -38,6 +52,9 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Posts, Users } = sequelize.models;
+
+
+
 
 // Aca vendrian las relaciones
 
