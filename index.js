8b655ed0d775453cc801e0d4b5d2d464b74cdbdf
gaston <ztonchyarg@hotmail.com>
2022-10-26 21:29:39 -0300

    deploy

--- index.js
+++ index.js
@@ -19,11 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3000, () => {
-    console.log(`%s listening at 3000`); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`% listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
