8d34c886eb4035d1d8bfd38d69a0611f4f082239
userJAMT <jmendez4tob@gmail.com>
2022-10-28 20:34:55 -0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const users = require('./routes/users.js');
 const posts = require('./routes/posts.js');
+const cors = require("cors");
 
 require('./db.js');
 
@@ -15,6 +16,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors());
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
