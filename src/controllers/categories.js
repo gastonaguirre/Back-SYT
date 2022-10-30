8e91d1192ed8c82e7d3693b19d602be87882def3
Jose Mendez <jmendez4tob@gmail.com>
2022-10-29 23:41:01 -0300

    Update categories.js

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,13 +1,5 @@
-const  axios  = require("axios");
 const { Categories } = require("../db.js");
 
-// let DbTypes = await Types.findAll().catch(e => undefined)
-//   if(!DbTypes.length){
-//     const types = await axios.get("https://pokeapi.co/api/v2/type").catch(e=>{
-//     return {message:'No logro acceder a la API'}});
-//     const typesData= types.data.results.map(e => e.name);
-//     for(let i = 0; i < typesData.length; i++){
-//       await Types.create({ name: typesData[i] })
 const getAllCategories = async (req, res) => {
     try {
      const allc = await Categories.findAll(
