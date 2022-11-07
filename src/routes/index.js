9bda97f72c69078117caee6ed4af105a4080bbc5
userJAMT <jmendez4tob@gmail.com>
2022-11-07 12:51:53 -0300

    algo mas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,13 @@
 const { Router } = require('express');
-const {Admin} = require("../db")
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const Users = require('./users')
 const Posts = require('./posts')
 const Categories = require('./categories')
-const userInactivo =require('./userInactivo')
 const Premium = require('./premium')
 const router = Router();
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 router.use('/users', Users)
 router.use('/posts', Posts)
 router.use('/categories', Categories)
-router.use('/userInactivo',userInactivo)
 router.use('/premium', Premium)
 
-
-router.post("/admin", async(req, res) => {
-    let {usuario, email, contraseña} = req.body;
-  try{
-    let nuevoAdmin = await Admin.create({usuario, email, contraseña})
-    res.status(200).send("se creo el admin")
-   } catch (err) {
-     console.log(err);
-     res.status(404).send("admin no creado")
-    }
-  });
 module.exports = router;
