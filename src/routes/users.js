bd4e9b9ea79403cfa1bdd7a25b52e4442ba6da63
Luis Condori <luis2003nb@gmail.com>
2022-11-08 18:09:52 -0300

    ahha

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,26 +1,19 @@
 const { Router } = require("express");
 const {
   getUsers,
-  // deleteIdUser,
-  postUser,
   perfilUser,
   editUser,
-  inicioSesion,
   deleteLogico,
-  deleteIdUser,
   findOrCreate,
-  perfilUser,
-  editUser,
+ 
+  
 } = require("../controllers/users");
 
 const router = Router();
 
 router.get("/", getUsers);
 router.get("/:idUser", perfilUser);
-// router.delete("/:idDelete", deleteIdUser);
 router.delete("/:id", deleteLogico)
-router.post("/", postUser);
-router.delete("/:idDelete", deleteIdUser);
 router.post("/", findOrCreate);
 router.patch("/:id", editUser);
 
