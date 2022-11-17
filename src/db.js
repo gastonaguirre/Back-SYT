64f39e5774cbf7cc41ee65d250b45ed6be5da29b
ednacoveja <ovejerocande@gmail.com>
2022-11-17 01:41:30 -0300

    lo que falto

--- src/db.js
+++ src/db.js
@@ -2,12 +2,34 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { PGDATABASE,PGHOST ,PGPASSWORD ,PGPORT, PGUSER} = process.env;
-const pg = require("pg")
-let sequelize =  new Sequelize(`postgresql://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${ PGDATABASE }`, {
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, } = process.env;
+
+let sequelize =
+  process.env.NODE_ENV === 'production'
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: 'postgres',
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          },
+          keepAlive: true
+        },
+        ssl: true
+      })
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/syt`, {
         logging: false,
         native: false,
-        dialectModule:pg
       });
 const basename = path.basename(__filename);
 
