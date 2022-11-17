f3b2c1c66aff659b4d2869abe799f1d5bebf9ac1
Luis Condori <luis2003nb@gmail.com>
2022-11-17 00:18:11 -0300

    terminando

--- src/routes/premium.js
+++ src/routes/premium.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const mercadopago = require("mercadopago")
+
 const Stripe = require("stripe");
 const { Users } = require("../db");
 const {sendMailPremium} = require("../controllers/mailer")
-const { MPAGO_ACCESS_TOKEN } = process.env;
-mercadopago.configure({
-  access_token: MPAGO_ACCESS_TOKEN,
-})
+
 const stripe = new Stripe("sk_test_51M4XYbIEql9X77EBzKonxJpaGRoqHvufT3XLhV2sWbGUFenpZ6tsLPMW3Jn5uMirPe96T7OacjD65fsdr0LSwm7M00qKOcsbaT")
 
 async function hacerPremium(e){
@@ -27,32 +24,7 @@ async function hacerPremium(e){
   }
 }
 
-router.post("/pay", async (req, res) => {
-  try {
-    let preference = {
-      items: [
-        {
-          title: "Premium",
-          unit_price: 2,
-          quantity: 1,
-        },
-      ],
-      back_urls: {
-        "success": "http://localhost:3001/premium/feedback",
-        "failure": "http://localhost:3001/premium/feedback",
-        "pending": "http://localhost:3001/premium/feedback"
-      },
-      auto_return: "approved",
-    };
-    const respon = await mercadopago.preferences.create(preference);
-    const preferenId = respon.body.id;
-    res.json({ preferenId })
 
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
-  // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
-})
 
 
 router.post("/api/checkout", async (req, res) => {
